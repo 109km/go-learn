5b8ebfcb55f677c8e3afe2bbe03f3fa285085576
xinheng.shao <shaoxinheng@gmail.com>
2019-04-25 18:41:54 +0800

    up

---
+++ closures/closures.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func autoPlus(gap int) func() int {
+	var i = 0
+	return func() int {
+		i = i + gap
+		return i
+	}
+}
+
+func main() {
+	var tick = autoPlus(1)
+	fmt.Println("add 1 per time", tick())
+	fmt.Println("add 1 per time", tick())
+
+	var anotherTick = autoPlus(2)
+	fmt.Println("add 2 per time:", anotherTick())
+	fmt.Println("add 2 per time:", anotherTick())
+}
