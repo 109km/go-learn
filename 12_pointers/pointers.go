f1483ee3126470ef4a9af44d4349f08dc7cbe06d
Kv.se7en <shaoxinheng@gmail.com>
2019-12-15 22:52:11 +0800

    up

--- 12_pointers/pointers.go
+++ 12_pointers/pointers.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+type user struct {
+	name string
+	age  int
+}
+
 func zeroval(ival int) {
 	ival = 0
 }
@@ -9,17 +14,30 @@ func zeroval(ival int) {
 func zeropointer(iptr *int) {
 	*iptr = 0
 }
-
+func escaspeToHeap() *user {
+	u := user{
+		name: "Hello",
+		age:  10,
+	}
+	return &u
+}
 func main() {
 
 	i := 1
+	j := 2
+	l := 3
 
 	zeroval(i)
-	fmt.Println(i)
+	fmt.Println(i) // Still 1
 
 	iptr := &i
 	zeropointer(iptr)
 
-	fmt.Println(i)
+	user1 := escaspeToHeap()
+
+	fmt.Printf("%p\n", user1)
+
 	fmt.Println(&i)
+	fmt.Println(&j)
+	fmt.Println(&l)
 }
--- 15_structs/structs.go
+++ 15_structs/structs.go
@@ -2,17 +2,14 @@ package main
 
 import "fmt"
 
-type Vertex struct {
-	X, Y int
+type vertex struct {
+	flag    bool
+	counter int16
+	pi      float32
 }
 
-var (
-	v1 = Vertex{1, 2}  // has type Vertex
-	v2 = Vertex{X: 1}  // Y:0 is implicit
-	v3 = Vertex{}      // X:0 and Y:0
-	p  = &Vertex{1, 2} // has type *Vertex
-)
+var v1 vertex
 
 func main() {
-	fmt.Println(v1, v2, v3, p)
+	fmt.Printf("%+v\n", v1)
 }
