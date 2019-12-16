d57abb7e96753263639ca94d3ca2f3c1dd6af493
xinheng.shao <shaoxinheng@gmail.com>
2019-12-16 18:21:34 +0800

    up

--- 35_loop_performance/loop_performance.go
+++ 35_loop_performance/loop_performance.go
@@ -5,7 +5,13 @@ import (
 	"time"
 )
 
+/**
+ * In golang, loop1 is also much faster than loop2,
+ * just because the `i,j`'s order is different.
+ */
+
 func startLoop1(src [2000][2000]int, dest [2000][2000]int) {
+	start := time.Now().UnixNano()
 	i := 0
 	j := 0
 	for ; i < 2000; i++ {
@@ -13,8 +19,11 @@ func startLoop1(src [2000][2000]int, dest [2000][2000]int) {
 			dest[i][j] = src[i][j]
 		}
 	}
+	end := time.Now().UnixNano()
+	fmt.Println((end - start) / 1e3)
 }
 func startLoop2(src [2000][2000]int, dest [2000][2000]int) {
+	start := time.Now().UnixNano()
 	i := 0
 	j := 0
 	for ; j < 2000; j++ {
@@ -22,6 +31,8 @@ func startLoop2(src [2000][2000]int, dest [2000][2000]int) {
 			dest[i][j] = src[i][j]
 		}
 	}
+	end := time.Now().UnixNano()
+	fmt.Println((end - start) / 1e3)
 }
 
 var src1 [2000][2000]int
@@ -30,15 +41,6 @@ var src2 [2000][2000]int
 var dest2 [2000][2000]int
 
 func main() {
-
-	start2 := time.Now().UnixNano()
-	startLoop1(src2, dest2)
-	end2 := time.Now().UnixNano()
-	fmt.Println((end2 - start2) / 1e6)
-
-	start := time.Now().UnixNano()
 	startLoop1(src1, dest1)
-	end := time.Now().UnixNano()
-	fmt.Println((end - start) / 1e6)
-
+	startLoop2(src2, dest2)
 }
