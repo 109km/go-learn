cc1dadb9221b642472d68268c7e2499d068911f4
xinheng.shao <shaoxinheng@gmail.com>
2019-12-18 17:58:29 +0800

    up

--- 20.4_channel_deadlock/channel_deadlock.go
+++ 20.4_channel_deadlock/channel_deadlock.go
@@ -8,12 +8,11 @@ func main() {
 	ch = make(chan int)
 
 	go put(ch)
-
-	for {
-		// Here will always wait for the channel's value
-		// When there is no more value, this will be blocked
-		// But this is in main!
-		fmt.Println(<-ch)
+	// Here will always wait for the channel's value
+	// When there is no more value, this will be blocked
+	// But this is in main!
+	for i := range ch {
+		fmt.Println(i)
 	}
 	// This line won't run
 	fmt.Println("done")
@@ -22,4 +21,8 @@ func put(ch chan int) {
 	for i := 0; i < 100; i++ {
 		ch <- i
 	}
+	// If we close the channel manually,
+	// the `for-range` will detect the channel is closed or not,
+	// so it won't cause deadlock.
+	// close(ch)
 }
